69c46cc2e40aa0d3bb8c05461343a6bce69ab9cc
rccox52 <rccox52@gmail.com>
2016-11-23 18:58:25 -0600

    Remove connection string initialization from the DBI layer

--- SQLServerDB/Connection_String.cs
+++ SQLServerDB/Connection_String.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO; //Path
 
 
 namespace SQLServerDB
@@ -10,29 +9,6 @@ namespace SQLServerDB
 
         public static string ADO_ConnectionString { get; set; }
 
-        public static void Initialize_ADO_Connection_String()
-        {
-            System.Reflection.Assembly myAssembly = typeof(Connection_String).Assembly;
-            string strAssemblyLocation = Path.GetDirectoryName(myAssembly.Location);
-
-            string ADODB_DatabasePath = strAssemblyLocation + @"\assess.mdb";
-
-            //string strProvider_Jet = "Provider=Microsoft.Jet.OLEDB.4.0;";
-            string strProvider_ACE = "Provider=Microsoft.ACE.OLEDB.12.0;";
-            string strProvider = strProvider_ACE;
-
-            ADO_ConnectionString = "Provider = Microsoft.Jet.OLEDB.4.0;Data Source = " + ADODB_DatabasePath;
-
-            Console.WriteLine("ADO_ConnectionString=" + ADO_ConnectionString);
-        }
-        public static void Initialize_SQLServer_ConnectionString()
-        {
-            SQLServer_ConnectionString = @"Server=XPS8700\SQLEXPRESS2014;Database=CAPDevDb;" +
-                "Trusted_Connection=True;" +
-                "Integrated Security=true;";
-
-            Console.WriteLine("SQLServer_ConnectionString=" + SQLServer_ConnectionString);
-        }
     }//class Connection_String
 
     }//namespace
