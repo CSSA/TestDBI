871465a0d6b9361de20d261abb543be7042d412d
rccox52 <rccox52@gmail.com>
2016-11-25 07:25:12 -0600

    Save updates- Util.cs

---
+++ Util.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDBI
+{
+   public class Util
+    {
+        //----------------------------------------------------------------------------------
+        public static void pause()
+        {
+            Console.WriteLine("pausing...");
+            Console.ReadKey();
+        }//void pause()
+        public static void pause(String msg)
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine("pausing...");
+            Console.ReadKey();
+        }//void pause()
+    }
+}
